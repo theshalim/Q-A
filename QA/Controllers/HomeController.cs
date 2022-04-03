275870110b5b6f57c98ad79f0a8f1ada9e937ca9
theshalim <shalim.reza@yahoo.com>
2022-04-03 07:40:06 +0600

    ss

--- QA/Controllers/HomeController.cs
+++ QA/Controllers/HomeController.cs
@@ -19,7 +19,10 @@ namespace QA.Controllers
         {
             return View();
         }
-
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
